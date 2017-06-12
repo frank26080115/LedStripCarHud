92c564a186053e9eef4ab8a7092b911487c2fe8e
Frank Zhao <frank.zhao.main@gmail.com>
2017-06-12 16:28:53 -0700

    canbus.ino better readability and flexibility

--- firmware/LedStripCarHud/canbus.ino
+++ firmware/LedStripCarHud/canbus.ino
@@ -1,12 +1,54 @@
-bool canbus_read(int* kmh, int* rpm, int* pedal)
+bool canbus_readAll(int* kmh, int* rpm, int* pedal)
 {
-	CAN_message_t txmsg;
-	
+	bool success = 1;
+	uint8_t tmp[2];
+	int res;
+
+	success &= canbus_readOne(ECUPID_SPEED, tmp);
+	res = tmp[0];
+	*kmh = res;
+
+	success &= canbus_readOne(ECUPID_ENGINE_RPM, tmp);
+	res = ((tmp[0]*256) + tmp[1])/4;
+	*rpm = res;
+
+	success &= canbus_readOne(ECUPID_PEDAL, tmp);
+	res = tmp[0];
+	*pedal = res; // ranged 0 to 255, 255 means 100%
+
+	return success;
+}
+
+bool canbus_readOne(uint8_t pid, uint8_t* result)
+{
+	CAN_message_t txmsg, rxmsg;
+	char success = 0;
+	uint32_t tmr;
+	canbus_ecuRequestFill(&txmsg, pid);
+	CANn.write(txmsg);
+	tmr = millis();
+	while ((millis() - tmr) < CANBUS_TIMEOUT && success == 0)
+	{
+		while (CANn.available())
+		{
+			CANn.read(rxmsg);
+			if (rxmsg.id == ECU_PID_REPLY)
+			{
+				if (rxmsg.buf[2] == pid)
+				{
+					result[0] = rxmsg.buf[3];
+					result[1] = rxmsg.buf[4];
+					success = 1;
+				}
+			}
+		}
+	}
+	return (bool)success;
 }
 
 void canbus_ecuRequestFill(CAN_message_t* x, uint8_t pid)
 {
-	x->id = 0x07DF; // PID_REQUEST
+	x->id = ECU_PID_REQUEST;
 	x->rtr = 0;
 	x->len = 8;
 	x->buf[0] = 0x02;
@@ -17,11 +59,6 @@ void canbus_ecuRequestFill(CAN_message_t* x, uint8_t pid)
 	x->buf[5] = 0x00;
 	x->buf[6] = 0x00;
 	x->buf[7] = 0x00;
-}
-
-int32_t canbus_parseRPM(CAN_message_t* msg)
-{
-	return ((msg->buf[3]*256) + msg->buf[4])/4; // 
 }
 
 bool canbus_scan(void)
@@ -35,31 +72,13 @@ bool canbus_scan(void)
 	Serial.printf("Begin CAN bus scanning...\n");
 	for (baud = 250000; baud <= 500000; baud += 250000)
 	{
-		Can0.begin(250000);
+		CANn.begin(250000);
 		for (attempts = 0; attempts < 3 && success == 0; attempts++)
 		{
-			success = 0;
-			canbus_ecuRequestFill(&txmsg, 0x0C);
-			Can0.write(txmsg);
-			tmr = millis();
-			while ((millis() - tmr) < 1000)
+			uint8_t tmp[2];
+			if ((success = canbus_readOne(ECUPID_ENGINE_RPM, tmp)) != 0)
 			{
-				while (Can0.available())
-				{
-					Can0.read(rxmsg);
-					if (rxmsg.id == 0x07E8)
-					{
-						if (rxmsg.buf[2] == 0x0C)
-						{
-							result = canbus_parseRPM(&rxmsg);
-							success = 1;
-						}
-					}
-				}
-				if (success != 0)
-				{
-					break;
-				}
+				result = ((tmp[0]*256) + tmp[1])/4;
 			}
 		}
 		if (success != 0)
@@ -67,9 +86,14 @@ bool canbus_scan(void)
 			Serial.printf("CAN bus at %u baud got ECU reply, RPM = %d\n", baud, result);
 			return 1;
 		}
-		Can0.end();
+		CANn.end();
 		Serial.printf("CAN bus at %u baud FAILED to get reply\n", baud);
 		delay(500);
 	}
 	return 0;
+}
+
+void canbus_init(void)
+{
+	CANn.begin(250000); // change the baud here
 }
