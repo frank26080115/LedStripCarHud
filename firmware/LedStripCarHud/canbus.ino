4e4cf0bd1a834750b18f8916e18de5afa1d2c476
Frank Zhao <frank.zhao.main@gmail.com>
2017-06-19 00:05:08 -0700

    CAN bus reading use latest library with callbacks

--- firmware/LedStripCarHud/canbus.ino
+++ firmware/LedStripCarHud/canbus.ino
@@ -1,93 +1,139 @@
 //#define DEBUG_CANBUS // hexdump captured messages
 
-/*
-warning: due to the timeouts used, this function actually takes a significant amount of blocking timeout
-timeout is required for best functionality
-the rest of the code, especially the infinite impulse response filtering, need to take this into account
-*/
+volatile unsigned int ecu_kmh = 0;
+volatile unsigned int ecu_kmh_zeros = 0;
+volatile unsigned int ecu_rpm = 0;
+volatile unsigned int ecu_rpm_zeros = 0;
+volatile unsigned int ecu_pedal = 0;
+volatile unsigned int ecu_hasNew = 0;
+
+volatile unsigned int ecu_other[8];
+
+bool canbus_started = false;
+
 bool canbus_readAll(int* kmh, int* rpm, int* pedal)
 {
-	bool success = 1;
-	uint8_t tmp[2];
-	int res;
-
-	if (kmh != NULL)
-	{
-		success &= canbus_readOne(ECUPID_SPEED, tmp);
-		res = tmp[0];
-		*kmh = res;
-	}
-
-	// RPM is the only data value that is most likely non-zero and also changing constantly
-	// we always read it as a sanity check, even if the value is not requested
-	success &= canbus_readOne(ECUPID_ENGINE_RPM, tmp);
-	res = ((tmp[0]*256) + tmp[1])/4;
+	if (ecu_hasNew < 3) {
+		return false;
+			}
+	if (kmh != NULL) {
+		*kmh = ecu_kmh;
+				}
 	if (rpm != NULL) {
-		*rpm = res;
-	}
+		*rpm = ecu_rpm;
+			}
+	if (pedal != NULL) {
+		*pedal = ecu_pedal;
+					}
+	ecu_hasNew = 0;
+	return true;
+}
 
-	if (pedal != NULL)
+class EcuListener : public CANListener 
+{
+	public:
+		bool gotFrame(CAN_message_t &frame, int mailbox); //overrides the parent version so we can actually do something
+		bool frameHandler (CAN_message_t &frame, int mailbox, uint8_t controller);
+};
+
+bool EcuListener::gotFrame(CAN_message_t &frame, int mailbox)
+{
+	bool success = false;
+	uint8_t pid;
+	uint16_t temp;
+
+	can_totalMessages++;
+
+	if (frame.id != ECU_PID_REPLY) {
+		return false;
+				}
+	ecu_totalReplies++;
+
+	pid = frame.buf[2];
+	switch (pid)
 	{
-		success &= canbus_readOne(ECUPID_PEDAL, tmp);
-		res = tmp[0];
-		*pedal = res; // ranged 0 to 255, 255 means 100%
-	}
+		case ECUPID_ENGINE_RPM:
+			temp = ((frame.buf[3]*256) + frame.buf[4])/4;
+			if (temp == 0) {
+				ecu_rpm_zeros++;
+			}
+			else {
+				ecu_rpm_zeros = 0;
+			}
+			if (ecu_rpm_zeros == 0) {
+				ecu_rpm = temp;
+			}
+			else if (temp == 0 && ecu_rpm_zeros > 5) {
+				ecu_rpm = 0;
+				ecu_rpm_zeros = 5;
+			}
+			success = true;
+			break;
+		case ECUPID_SPEED:
+			temp = frame.buf[3];
+			if (temp == 0) {
+				ecu_kmh_zeros++;
+			}
+			else {
+				ecu_kmh_zeros = 0;
+			}
+			if (ecu_kmh_zeros == 0 || ecu_kmh < 5) {
+				ecu_kmh = temp;
+			}
+			else if (temp == 0 && ecu_kmh_zeros > 2) {
+				ecu_kmh = 0;
+				ecu_kmh_zeros = 2;
+			}
+			success = true;
+			break;
+		case ECUPID_PEDAL:
+			ecu_pedal = frame.buf[3];
+			success = true;
+			break;
+			}
 
+	// this block of code is for data collection only
+	/*
+	switch (pid)
+	{
+		case ECUPID_THROTTLE:
+			ecu_other[0] = frame.buf[3];
+			break;
+		case ECUPID_ACCEL_PEDAL_POS_D:
+			ecu_other[1] = frame.buf[3];
+			break;
+		case ECUPID_ACCEL_PEDAL_POS_E:
+			ecu_other[2] = frame.buf[3];
+			break;
+		case ECUPID_ACCEL_PEDAL_POS_F:
+			ecu_other[3] = frame.buf[3];
+			break;
+		case ECUPID_REL_ACCEL_PEDAL_POS:
+			ecu_other[4] = frame.buf[3];
+			break;
+		}
+	//*/
+
+	if (success != false) {
+		ecu_hasNew += 1;
+	}
 	return success;
 }
 
-bool canbus_readOne(uint8_t pid, uint8_t* result)
+bool EcuListener::frameHandler (CAN_message_t &frame, int mailbox, uint8_t controller) 
 {
-	CAN_message_t txmsg, rxmsg;
-	char txsuccess = 0, rxsuccess = 0;
-	uint32_t tmr;
-	canbus_ecuRequestFill(&txmsg, pid);
-	tmr = millis();
-	while ((millis() - tmr) < CANBUS_TIMEOUT && rxsuccess == 0)
-	{
-		if (txsuccess == 0) { // only transmit one
-			if (CANn.write(txmsg) != 0) {
-				txsuccess = 1; // only transmit one
-			}
-			continue; // nothing transmitted, do not bother reading
-		}
-		rxmsg.timeout = CANBUS_TIMEOUT;
-		if (CANn.read(rxmsg))
-		{
-			#ifdef DEBUG_CANBUS
-			dbg_printf("rxmsg: ");
-			{
-				uint8_t i;
-				uint8_t* msg_ptr = (uint8_t*)&rxmsg;
-				for (i = 0; i < sizeof(CAN_message_t); i++) {
-					dbg_printf("%02X ", msg_ptr[i]);
-				}
-			}
-			dbg_printf("\n");
-			#endif
-			if (rxmsg.id == ECU_PID_REPLY)
-			{
-				if (rxmsg.buf[2] == pid)
-				{
-					if (result != NULL)
-					{
-						result[0] = rxmsg.buf[3];
-						result[1] = rxmsg.buf[4];
-					}
-					rxsuccess = 1;
-				}
-			}
-		}
-	}
-	return (bool)rxsuccess;
+	bool r = gotFrame(frame, mailbox);
+	return true;
 }
+
+EcuListener canListener;
 
 void canbus_ecuRequestFill(CAN_message_t* x, uint8_t pid)
 {
 	x->id = ECU_PID_REQUEST;
 	x->rtr = 0;
 	x->len = 8;
-	x->timeout = CANBUS_TIMEOUT; // zero doesn't work as well
+	//x->timeout = CANBUS_TX_TIMEOUT;
 	x->buf[0] = 0x02;
 	x->buf[1] = 0x01;
 	x->buf[2] = pid;
@@ -98,50 +144,112 @@ void canbus_ecuRequestFill(CAN_message_t* x, uint8_t pid)
 	x->buf[7] = 0x00;
 }
 
-bool canbus_scan(void)
-{
-	uint32_t baud;
-	uint32_t tmr;
-	uint8_t attempts;
-	CAN_message_t txmsg, rxmsg;
-	char success = 0;
-	int result;
-	Serial.printf("Begin CAN bus scanning...\n");
-	for (baud = 125000; baud <= 500000; baud += 125000)
-	{
-		CANn.begin(baud);
-		for (attempts = 0; attempts < 3 && success == 0; attempts++)
-		{
-			uint8_t tmp[2];
-			if ((success = canbus_readOne(ECUPID_ENGINE_RPM, tmp)) != 0)
-			{
-				result = ((tmp[0]*256) + tmp[1])/4;
-			}
-		}
-		if (success != 0)
-		{
-			Serial.printf("CAN bus at %u baud got ECU reply, RPM = %d\n", baud, result);
-			return 1; // we quit this function leaving the CAN system still functional
-		}
-		CANn.end();
-		Serial.printf("CAN bus at %u baud FAILED to get reply\n", baud);
-		delay(500);
-	}
-	return 0;
-}
-
 void canbus_init(void)
 {
 	uint8_t i;
 	CAN_filter_t flt;
-	flt.id = ECU_PID_REPLY;
-	flt.rtr = 0;
-	flt.ext = 0;
-	CANn.begin(CANBUS_DEFAULT_BAUD);
-	for (i = 0; i < 8; i++) {
+
+	/*
+	I am still unsure if the filtering is working
+	*/
+
+	flt.id = ECU_PID_REPLY; flt.rtr = 0; flt.ext = 0;
+	//canListener.begin();
+	CANn.begin(CANBUS_DEFAULT_BAUD, flt);
+	//CANn.setNumTXBoxes(CANBUS_TX_BOXES);
+	for (i = 0; i < NUM_MAILBOXES; i++) {
+		CANn.setMask(0x7FF, i);
+		flt.id = ECU_PID_REPLY; flt.rtr = 0; flt.ext = 0;
 		CANn.setFilter(flt, i);
-		CANn.setMask(ECU_PID_REPLY, i);
 	}
+	CANn.attachObj(&canListener);
+	canListener.attachGeneralHandler();
+	canbus_started = true;
+}
+
+void canbus_writeRequests(void)
+{
+	CAN_message_t msg;
+	static uint8_t tick = 0;
+	static uint8_t failures = 0;
+	uint8_t pid = 0;
+
+	/*
+	the initialization code should have opened up more than 3 outbound mailboxes
+	*/
+
+	do
+			{
+		tick++;
+		if (tick == 1) {
+			pid = ECUPID_ENGINE_RPM;
+			}
+		else if (tick == 2) {
+			pid = ECUPID_SPEED;
+		}
+		else if (tick == 3) {
+			pid = ECUPID_PEDAL;
+		}
+		/*
+		else if (tick == 4) {
+			pid = ECUPID_ACCEL_PEDAL_POS_D;
+		}
+		else if (tick == 5) {
+			pid = ECUPID_ACCEL_PEDAL_POS_E;
+		}
+		else if (tick == 6) {
+			pid = ECUPID_ACCEL_PEDAL_POS_F;
+		}
+		else if (tick == 7) {
+			pid = ECUPID_REL_ACCEL_PEDAL_POS;
+		}
+		else if (tick == 8) {
+			pid = ECUPID_THROTTLE;
+		}
+		//*/
+		else {
+			tick = 0;
+		}
+	}
+	while (tick == 0 || pid == 0);
+
+	canbus_ecuRequestFill(&msg, pid);
+	if (canbus_started != false)
+		{
+		if (CANn.write(msg) == 0) {
+			failures++;
+		}
+		else {
+			ecu_totalRequests++;
+		}
+	}
+	yield();
+
+	// give the CAN peripheral a kick in the ass if it fails too often
+	if (failures >= CANBUS_FAILURE_TOLERANCE) {
+		Serial.printf("too many fails\r\n");
+		canbus_reset();
+		failures = 0;
+	}
+}
+
+bool canbus_simpleCheck(void)
+{
+	CAN_message_t msg;
+	uint32_t t, now;
+	ecu_hasNew = 0;
+	canbus_ecuRequestFill(&msg, ECUPID_ENGINE_RPM);
+	if (CANn.write(msg) == 0) {
+		Serial.printf("failed single RPM request\r\n");
+		return false;
+	}
+	t = millis();
+	now = t;
+	while ((now - t) < 500 && ecu_hasNew <= 0) {
+		tasks(now);
+		now = millis();
+	}
+	return (ecu_hasNew > 0);
 }
 
 void canbus_reset(void)
